07900ac1ddc2885117530fb3b64413e2c3f32229
Mochizue-RBT <raventejada1929@gmail.com>
2025-06-07 17:55:37 +0800

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,28 @@
+// // JavaScript Practice
+
+// // First variables
+// let custname = prompt("Hello and welcome to Mochi! What's your name?");
+// let authname = "Raven";
+
+// // Variable outputs
+// alert("Hello " + custname + " and welcome to mochi. The author of this company is " + authname + "!");
+
+// // Reversed variables
+// let reqprod = prompt("What do you need today for your style?");
+//     crreverse = reqprod;
+//     reqprod = custname;
+//     custname = crreverse;
+
+// // Variable outputs
+// alert("Okay!, " + custname + " wants " + reqprod);
+// alert("Oh, I'm sorry let me rephrase that real quick");
+// alert("Okay!, " + reqprod + " wants " + crreverse);
+
+// // Third variables
+// let petname = "mochi";
+// alert("Please say hello to my cute dog " + petname + ", " + reqprod + "!");
+
+
+
+
+
